1b9fae01459bf8af12f035c25e3cfca136079f84
James Corwin <jamesjamesjac@gmail.com>
2012-08-24 03:41:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cultureshock::Application.config.secret_token = '32837918296e9205b1afe4e28ae37c3bc2ef698a4f9ad5380fd36edf2069fae571d15460fa92e2d27218d0f94736cf1c0f0049dea0b3f09905c5271bbc3d5787'
