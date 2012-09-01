bd3ee45895396f7aeda4b327a0c29f421bac66f5
James Corwin <jamesjamesjac@gmail.com>
2012-08-31 22:08:37 -0700

    Not much change.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Culture Shocker | Home")
     end
   end
 
@@ -14,6 +21,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Culture Shocker | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -21,6 +34,12 @@ describe PagesController do
       get 'about'
       response.should be_success
     end 
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Culture Shocker | About")
+    end
   end
 
 end
